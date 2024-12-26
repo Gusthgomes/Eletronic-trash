1fcb6df5dc551a22c904cae21377ffdc1ac8bfa8
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2024-12-26 17:06:13 -0300

    feat: add quiz link

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
 import { Card, CardContent } from "@/components/ui/card"
-import { Trash, TriangleAlert} from "lucide-react"
+import { Trash, TriangleAlert, ThumbsUp, Dices} from "lucide-react"
 import Link from "next/link";
 
 const Header = () => {
@@ -12,13 +12,24 @@ const Header = () => {
                 </Link>
 
                 <h1 className="font-bold text-xl font-mono text-green-700">
-                    Lixo Eletrônico
+                    E-lixo
                 </h1>
             </div>
 
-            <div className="flex items-center justify-between gap-2">
-                <Link href="/risks">
+            <div className="flex items-center justify-between gap-2 space-x-4">
+                <Link href="/risks" className="flex items-center gap-1 text-green-600">
+                    Riscos
                     <TriangleAlert size={24} className="text-green-500" />
+                </Link>
+
+                <Link href="/recycling" className="flex items-center gap-1 text-green-600">
+                    Boas práticas
+                    <ThumbsUp size={24} className="text-green-500" />
+                </Link>
+
+                <Link href="/quiz" className="flex items-center gap-1 text-green-600">
+                    Quiz
+                    <Dices size={24} className="text-green-500" />
                 </Link>
             </div>
             </CardContent>
