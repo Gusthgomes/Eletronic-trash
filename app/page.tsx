c1d18a2f2994bfe2f2ff1f0a4acc139acefbc64d
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2024-12-26 14:40:51 -0300

    chore: project setup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center w-full text-red-300">
+      <Button>Página inicial</Button>
+    </div>
+  );
+}
