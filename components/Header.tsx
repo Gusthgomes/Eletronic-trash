570c935d22deeea780b243cede4fbd8f8ed29ee6
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2024-12-26 15:43:40 -0300

    feat: add risk link

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
 import { Card, CardContent } from "@/components/ui/card"
-import { Trash} from "lucide-react"
+import { Trash, TriangleAlert} from "lucide-react"
 import Link from "next/link";
 
 const Header = () => {
@@ -8,12 +8,18 @@ const Header = () => {
             <CardContent className="p-5 justify-between flex flex-row  items-center">
             <div className="flex items-center gap-3">
                 <Link href="/">
-                    <Trash size={24} className="fill-green-500" />
+                    <Trash size={24} className="text-green-500" />
                 </Link>
 
                 <h1 className="font-bold text-xl font-mono text-green-700">
                     Lixo Eletrônico
                 </h1>
+            </div>
+
+            <div className="flex items-center justify-between gap-2">
+                <Link href="/risks">
+                    <TriangleAlert size={24} className="text-green-500" />
+                </Link>
             </div>
             </CardContent>
         </Card>
