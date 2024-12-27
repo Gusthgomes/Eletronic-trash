2bc94e5137842ca52d3f092965289cfdf1c5ef90
Gustavo Gomes <gusthgomesdarosa@gmail.com>
2024-12-27 17:08:39 -0300

    chore: add title of page

--- app/quiz/page.tsx
+++ app/quiz/page.tsx
@@ -1,5 +1,9 @@
 import { EWasteQuiz } from '@/components/EWasteQuiz'
 
+export const metadata = {
+  title: "Quiz"
+}
+
 export default function QuizPage() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-green-50 to-blue-50 py-12 px-4 sm:px-6 lg:px-8">
